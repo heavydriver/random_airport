e77dea8cc4f5492273ad14048e568b8b98545825
Varun Mange <mangevarun2006@gmail.com>
2022-02-24 00:25:35 -0600

    final

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const cron = require("node-cron");
 const shell = require("shelljs");
 
-cron.schedule("* * * * *", () => {
+cron.schedule("0 */4 * * *", () => {
   console.log("running a task every minute");
   if (shell.exec("node ./helper/tweet.js").code !== 0) {
     console.log("Something went wrong");
