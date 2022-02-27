5bb9607fd6322b64fd3aa92f98445cdc996c3487
Varun Mange <mangevarun2006@gmail.com>
2022-02-26 19:06:22 -0600

    execute every hour

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const shell = require("shelljs");
 
 console.log("Started");
 
-cron.schedule("0 */4 * * *", () => {
+cron.schedule("0 * * * *", () => {
   console.log("running a task every minute");
   if (shell.exec("node ./helper/tweet.js").code !== 0) {
     console.log("Something went wrong");
